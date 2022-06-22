4380368fb92e8910185447195328e4389585bd36
s2100628-Alex <alex.degerman.dev@gmail.com>
2022-06-22 03:39:43 +0300

    update product-details

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
 import { products } from '../products';
 import { CartService } from '../cart.service';
 
@@ -9,19 +10,23 @@ import { CartService } from '../cart.service';
   styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
+  products = products;
   product: any;
+
   constructor(
     private route: ActivatedRoute,
     private cartService: CartService
   ) {}
-  addToCart(product: any) {
-    window.alert('Your product has been added to the cart!');
-    this.cartService.addToCart(product);
-  }
 
   ngOnInit() {
+    console.log(this.products);
     this.route.paramMap.subscribe((params: any) => {
       this.product = products[+params.get('productId')];
     });
+  }
+
+  addToCart(product: any) {
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(product);
   }
 }
